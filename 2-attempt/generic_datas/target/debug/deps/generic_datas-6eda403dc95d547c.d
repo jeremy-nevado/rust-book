21c739f58981dd198e114b4702668cb8569881d4
Jeremy Nevado <jeremy@nevado.xyz>
2021-06-09 19:25:47 -0400

    Updating some useless stuff.

---
+++ 2-attempt/generic_datas/target/debug/deps/generic_datas-6eda403dc95d547c.d
@@ -0,0 +1,5 @@
+/home/jeremy/Coding/rust-book/2-attempt/generic_datas/target/debug/deps/generic_datas-6eda403dc95d547c.rmeta: src/main.rs
+
+/home/jeremy/Coding/rust-book/2-attempt/generic_datas/target/debug/deps/generic_datas-6eda403dc95d547c.d: src/main.rs
+
+src/main.rs:
