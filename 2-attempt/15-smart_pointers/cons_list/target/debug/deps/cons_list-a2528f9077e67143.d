03115689a6c4c5cde2f640f02b621c01f41ee661
Jeremy Nevado <jeremy@nevado.xyz>
2021-06-13 20:21:05 -0400

    Did some more stuff.

---
+++ 2-attempt/15-smart_pointers/cons_list/target/debug/deps/cons_list-a2528f9077e67143.d
@@ -0,0 +1,5 @@
+/Users/jeremynevado/Coding/rust-book/2-attempt/15-smart_pointers/cons_list/target/debug/deps/cons_list-a2528f9077e67143.rmeta: src/main.rs
+
+/Users/jeremynevado/Coding/rust-book/2-attempt/15-smart_pointers/cons_list/target/debug/deps/cons_list-a2528f9077e67143.d: src/main.rs
+
+src/main.rs:
